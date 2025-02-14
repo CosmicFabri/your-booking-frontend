c9e6d8c1ecb0b49b91059f24bb201322a5f3e094
Fabrizio Guzmán <fabrizzio86@outlook.es>
2025-02-14 14:01:57 -0600

    feat: The Home page is now responsive! Need to work on the Login and Signup pages next.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,18 +5,22 @@ import Button from './Button.vue';
 <template>
     <div class="bg-sky-200 xl:px-24 px-10 py-4 flex flex-row items-center shadow-xl justify-between">
         <!-- Logo image and app name -->
-        <div class="flex flex-row gap-x-4 items-center">
+        <div class="flex-1 flex flex-row gap-x-4 items-center">
             <img class="w-24 h-20 object-fill" src="../assets/img/Your_Booking_Logo_Shadow.png">
             <div class="flex flex-col">
-                <div class="text-sky-800 sm:text-3xl text-2xl font-bold">Your</div>
-                <div class="text-sky-800 sm:text-3xl text-2xl font-bold">Booking</div>
+                <div class="sm:block hidden text-sky-800 md:text-3xl sm:text-2xl font-bold">Your</div>
+                <div class="sm:block hidden text-sky-800 md:text-3xl sm:text-2xl font-bold">Booking</div>
             </div>
         </div>
 
         <!-- UACAM logo -->
-        <img src="../assets/img/UACAM_Logo.png" class="lg:flex w-68 h-20 -ml-28 lg:visible invisible object-fill">
+        <div class="flex-1 flex justify-center">
+            <img src="../assets/img/UACAM_Logo.png" class="lg:block w-56 h-16 hidden object-fill">
+        </div>
 
         <!-- 'My Bookings' button -->
-        <Button :text="'My Bookings'"></Button>
+        <div class="flex-1 flex justify-end">
+            <Button :text="'My Bookings'"></Button>
+        </div>
     </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,27 +4,44 @@ import Button from '@/components/Button.vue';
 
 <template>
     <div class="flex flex-row">
-        <!-- Engineering Faculty picture (left) -->
-        <div class="h-screen shadow-2xl" style="height: calc(100vh - 7rem);">
-            <img src="../assets/img/Home.jpeg" alt="" class="w-full h-full object-cover">
+        <!-- Engineering Faculty picture (left, visible on desktop) -->
+        <div class="md:block hidden h-screen shadow-2xl" style="height: calc(100vh - 7rem);">
+            <img src="../assets/img/Home_Vertical.jpeg" alt="" class="w-full h-full object-cover">
         </div>
 
-        <!-- Main view (Engineering Faculty logo, description, Book Now) -->
-        <div class="flex flex-col justify-center items-center gap-y-16 flex-1">
+        <!-- Main view (Engineering Faculty logo, description, Book Now; all three visible on desktop) -->
+        <div class="flex flex-col justify-center items-center md:mt-0 mt-12 lg:gap-y-16 gap-y-8 flex-1">
+
+            <!-- App and University logo (visible on phones) -->
+            <div class="sm:hidden flex flex-col items-center gap-y-12 -mb-8">
+                <!-- Your Booking logo (visible on phones) -->
+                <div class="flex flex-row gap-x-4 items-center">
+                    <img class="w-24 h-20 object-fill" src="../assets/img/Your_Booking_Logo_Shadow.png">
+                    <div class="flex flex-col items-center gap-y-2">
+                        <div class="text-sky-800 text-3xl font-extrabold">Your</div>
+                        <div class="text-sky-800 text-3xl font-extrabold">Booking</div>
+                    </div>
+                </div>
+    
+                <!-- UACAM logo (visible on phones) -->
+                <div class="w-72 h-36">
+                    <img src="../assets/img/UACAM_Logo.png">
+                </div>
+            </div>
             
             <!-- Engineering faculty logo -->
-            <div class="flex flex-row items-center gap-x-4">
-                <div class="w-36 h-36">
+            <div class="flex flex-row items-center gap-x-4 mx-8">
+                <div class="xl:w-36 xl:h-36 w-24 h-24">
                     <img src="../assets/img/FDI_Logo.png">
                 </div>
                 <div class="flex flex-col gap-y-2 items-center">
-                    <div class="font-bold text-6xl text-sky-900">Engineering</div>
-                    <div class="font-bold text-6xl text-sky-900">Faculty</div>
+                    <div class="font-bold xl:text-6xl text-3xl text-sky-900">Engineering</div>
+                    <div class="font-bold xl:text-6xl text-3xl text-sky-900">Faculty</div>
                 </div>
             </div>
 
             <!-- Description -->
-            <div class="max-w-3xl text-2xl text-justify italic">
+            <div class="max-w-3xl xl:text-2xl text-lg text-justify italic p-6 mx-12 border-gray-300 border-2 border-solid rounded-xl shadow-xl">
                 With Your Booking, you can easily book spaces from the Faculty of Engineering, of the
                 Autonomus University of Campeche. Manage your spaces and schedules in a convenient
                 way, and edit them whenever you want, up to 24 hours before the booking date.
@@ -32,6 +49,11 @@ import Button from '@/components/Button.vue';
 
             <!-- Book Now -->
             <Button :text="'Book now!'" :font-size="'2xl'"></Button>
+
+            <!-- Engineering Faculty picture (bottom, visible on phones) -->
+            <div class="md:hidden block w-full">
+                <img src="../assets/img/Home_Horizontal.jpeg">
+            </div>
         </div>
     </div>
 </template>

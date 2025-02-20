bd38aae63db720e8f3e62b2804e009ac8e9f8162
Fabrizio Guzmán <fabrizzio86@outlook.es>
2025-02-19 18:49:39 -0600

    feat: Añadido contenido a la vista de las reservaciones para el administrador. Posteriormente se implementará JSON Server para probar el llenado dinámico de las tablas.

---
+++ src/components/admin/BookRow.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    bookNumber: Number,
+    bookSpace: String,
+    bookUser: String,
+    bookSchedule: String
+})
+</script>
+
+<template>
+    <div class="flex flex-row">
+        <div class="px-4 py-2 w-52 border-2 border-black">{{ props.bookNumber }}</div>
+        <div class="px-4 py-2 w-52 border-2 border-black">{{ props.bookSpace }}</div>
+        <div class="px-4 py-2 w-52 border-2 border-black">{{ props.bookUser }}</div>
+        <div class="px-4 py-2 w-52 border-2 border-black">{{ props.bookSchedule }}</div>
+    </div>
+</template>
--- src/views/admin/AdminBookings.vue
+++ src/views/admin/AdminBookings.vue
@@ -1,10 +1,39 @@
 <script setup>
 import AdminSidebar from '@/components/admin/AdminSidebar.vue';
+import BookRow from '@/components/admin/BookRow.vue';
 </script>
 
 <template>
     <div class="flex flex-row">
+        <!-- Sidebar -->
         <AdminSidebar></AdminSidebar>
-        <h1>This is the admin bookings view</h1>
+
+        <!-- Main view -->
+        <div class="flex flex-col px-24 py-16 gap-y-12">
+
+            <!-- Title -->
+            <div class="text-3xl font-semibold">Reservaciones</div>
+
+            <div>Aquí se mostrará el calendario</div>
+
+            <!-- Table showing the bookings of the date selected -->
+            <div class="flex flex-col">
+
+                <!-- Table header -->
+                <div class="flex flex-row">
+                    <div class="px-4 py-2 w-52 bg-sky-200 border-2 border-black font-semibold">No. Reservación</div>
+                    <div class="px-4 py-2 w-52 bg-sky-200 border-2 border-black font-semibold">Espacio</div>
+                    <div class="px-4 py-2 w-52 bg-sky-200 border-2 border-black font-semibold">Usuario</div>
+                    <div class="px-4 py-2 w-52 bg-sky-200 border-2 border-black font-semibold">Horario</div>
+                </div>
+
+                <!-- Table contents -->
+                <BookRow
+                    :book-number="1"
+                    :book-space="'CIC-1'"
+                    :book-user="'José Carlos Aguilar Canepa'"
+                    :book-schedule="'09:00 - 11:00'"></BookRow>
+            </div>
+        </div>
     </div>
 </template>

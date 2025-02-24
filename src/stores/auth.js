26a9924cc697f15e9496915c75975725ee04f06b
AleksPrz <alexis4g2@outlook.com>
2025-02-23 21:26:01 -0600

    feat: added login and logout functions to auth.js

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,8 +1,9 @@
 import { defineStore } from "pinia"
 import { ref } from "vue"
+import { fetchData } from "@/api"
 
 export const useAuthStore = defineStore('auth', () => {
-    // token and user are necesary to preserve adta across browser sessions
+    // token and user are necesary to preserve data across browser sessions
     const token = ref(localStorage.getItem('token'))
     const user = ref(localStorage.getItem('user'))
 
@@ -10,11 +11,33 @@ export const useAuthStore = defineStore('auth', () => {
     const isAdmin = computed(() => user.value?.role === 'admin' || false) // If user is null, returns false
     
     const login = async (email, password) => {
+        try {
+            const options = {body: {email, password}}
+            const data = await fetchData('login', 'POST', options)
 
+            token.value = data.token
+            user.value = data.user
+
+            localStorage.setItem('token', data.token)
+            localStorage.setItem('user', JSON.stringify(data.user))
+        } catch(error) {
+            console.log(error)
+        }
     }
 
     const logout = async () => {
+        try {
+            const options = {headers: {'Authentication': `Bearer: ${token}`}}
+            const data = await fetchData('logout', 'POST', options)
 
+            token.value = null
+            user.value = null
+
+            localStorage.removeItem('token')
+            localStorage.removeItem('user')
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     return {
